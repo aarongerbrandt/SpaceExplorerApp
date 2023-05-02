9c4c75ddbb8f7aa3a69daac5ce79a683c059372e
aarongerbrandt <aarond.gerbrandt@gmail.com>
2023-05-02 16:06:43 -0500

    Added tab icons, more touches to APOD UI

--- app/src/main/java/com/example/finalproject/FragmentPagerAdapter.kt
+++ app/src/main/java/com/example/finalproject/FragmentPagerAdapter.kt
@@ -6,7 +6,7 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.finalproject.apod.fragments.ApodListFragment
 import com.example.finalproject.rover.fragments.RoverListFragment
 
-class MyViewPagerAdapter(activity: FragmentActivity, private val itemCount: Int): FragmentStateAdapter(activity) {
+class FragmentPagerAdapter(activity: FragmentActivity, private val itemCount: Int): FragmentStateAdapter(activity) {
     override fun getItemCount(): Int = itemCount
 
     override fun createFragment(position: Int): Fragment {
